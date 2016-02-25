29d2c6e6bcfa0b1fd21a43067364f86d1b1fcbc2
Ryder Schoon <rdschoon@iastate.edu>
2016-02-25 16:37:10 -0600

    working on recipe

--- src/cs362Cookbook/Recipe.java
+++ src/cs362Cookbook/Recipe.java
@@ -2,7 +2,10 @@ package cs362Cookbook;
 
 import java.util.ArrayList;
 import java.util.List;
-public class Recipe
+
+import Interfaces.Ingredient_I;
+import Interfaces.Recipe_I;
+public class Recipe implements Recipe_I
 {
 	int id;
 	boolean isFavorite;
@@ -11,5 +14,35 @@ public class Recipe
 	List<Category> categories = new ArrayList<Category>();
 	String name;
 	Rating rating = Rating.NONE;
+	@Override
+	public boolean editRecipe(int ID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean discardRecipe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Recipe_I copyRecipe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean addIngredient(Ingredient_I I) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean removeIngredient(Ingredient_I I) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public List<Ingredient_I> getIngredients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
