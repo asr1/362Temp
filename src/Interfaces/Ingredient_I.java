69350fca9a92a65f01643a877aca04212283369a
Branden Sammons <Carhugar1@live.com>
2016-02-25 17:49:32 -0600

    Began adding Database Support calls.
    More to come. First getter in Ingredient_I

--- src/Interfaces/Ingredient_I.java
+++ src/Interfaces/Ingredient_I.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface Ingredient_I {
 	
+	// Class diagram fragment
 	public List<Recipe_I> getRecipes();
 	public boolean removeRecipe(Recipe_I R);
 	public boolean addRecipe(Recipe_I R);
+	
+	// Database Support needed calls
+	public String getName();
 
 }
