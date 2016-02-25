0a64a55e2f60803d2e22dc7218302f9e7d7a681c
asr1 <alexandersrinehart@gmail.com>
2016-02-25 16:45:04 -0600

    Update and add JUnit tests

--- src/tests/CookbookTests.java
+++ src/tests/CookbookTests.java
@@ -5,13 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import cs362Cookbook.Cookbook_Controller;
+import cs362Cookbook.Ingredient;
 import cs362Cookbook.Recipe;
 
 public class CookbookTests
 {
 
 	@Test
-	public void addRecipeTest()
+	public void cookbookAddRecipeTest()
 	{
 		Cookbook_Controller cont = new Cookbook_Controller();
 		Recipe recipe = new Recipe();
@@ -20,13 +21,26 @@ public class CookbookTests
 	}
 
 	@Test
-	public void removeRecipeTest()
+	public void cookbookRemoveRecipeTest()
 	{
 		Cookbook_Controller cont = new Cookbook_Controller();
 		Recipe recipe = new Recipe();
 		int id = cont.addRecipe(recipe);
 		boolean success = cont.removeRecipe(id);
 		assertEquals(success, true);
+	}
+	
+	@Test
+	public void cookbookRemoveIngredientTest() 
+	{
+		Cookbook_Controller cont = new Cookbook_Controller();
+		Recipe recipe = new Recipe();
+		Ingredient ing = new Ingredient();
+		recipe.addIngredient(ing);
+		boolean result = cont.removeIngredient(ing.getName());
+		boolean contains = recipe.getIngredients().contains(ing);
+		assertTrue(result);
+		assertFalse(contains);
 	}
 	
 }
---
+++ src/tests/IngredientTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IngredientTests
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
