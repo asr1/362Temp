59e4c25f931faa05ce957cf303a60a3c4039fcaa
Branden Sammons <Carhugar1@live.com>
2016-03-21 09:57:44 -0500

    Updated all the Interfaces to include new changes.
     - Changes in Database_Support:
     -- Deletes now need the object.
     -- added way to get Ingredient by id.
    
     - Changes in Ingredient:
     -- GetRecipes takes the database object.
     -- some database support calls added.
    
     - Changes in Recipe:
     -- database support call added.

--- src/Interfaces/Database_Support_I.java
+++ src/Interfaces/Database_Support_I.java
@@ -4,9 +4,10 @@ public interface Database_Support_I {
 
 	public int putRecipe(Recipe_I R);
 	public Recipe_I getRecipe(int ID);
-	public boolean deleteRecipe(int ID);
+	public boolean deleteRecipe(Recipe_I R);
 	public boolean putIngredient(Ingredient_I I);
 	public Ingredient_I getIngredient(String name);
-	public boolean deleteIngredient(String name);
+	public Ingredient_I getIngredient(int ID);
+	public boolean deleteIngredient(Ingredient_I I);
 	
 }
--- src/Interfaces/Ingredient_I.java
+++ src/Interfaces/Ingredient_I.java
@@ -5,11 +5,12 @@ import java.util.List;
 public interface Ingredient_I {
 	
 	// Class diagram fragment
-	public List<Recipe_I> getRecipes();
+	public List<Recipe_I> getRecipes(Database_Support_I db);
 	public boolean removeRecipe(Recipe_I R);
 	public boolean addRecipe(Recipe_I R);
 	
 	// Database Support needed calls
 	public String getName();
+	public Integer getID();
 
 }
