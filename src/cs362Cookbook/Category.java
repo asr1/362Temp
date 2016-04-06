8e400c7579421440ebdab7c6cd7dbcfb42d10dc2
Branden Sammons <Carhugar1@live.com>
2016-04-06 11:38:48 -0500

    Fixed Category.
    New implementation to reflect recent Interface changes.
    
    Also added a constructor for the database support object.

--- src/cs362Cookbook/Category.java
+++ src/cs362Cookbook/Category.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Interfaces.Category_I;
+import Interfaces.Database_Support_I;
 import Interfaces.Ingredient_I;
+import Interfaces.Recipe_I;
 
 public class Category implements Category_I
 {
 	String name;
-	List<Recipe> recipes;
+	int id;
+	List<Integer> Recipes;
 	
 	/**
 	 * Public constructor for a Category
@@ -17,12 +20,51 @@ public class Category implements Category_I
 	 */
 	public Category(String name) {
 		this.name = name;
-		this.recipes = new ArrayList<Recipe>();
+		this.Recipes = new ArrayList<Integer>();
+		this.id = -1;
+	}
+	
+	/**
+	 * Constructor for Database_Support
+	 * @param id
+	 * @param name
+	 */
+	public Category(int id, String name, List<Integer> Recipes) {
+		this(name);
+		this.id = id;
+		this.Recipes = Recipes;
+	}
+	
+
+	@Override
+	public boolean removeRecipe(Recipe_I R) {
+		Integer i = R.getID();
+		return Recipes.remove(i);
 	}
 
 	@Override
-	public boolean removeRecipe(Recipe rec)
-	{
-		return recipes.remove(rec);
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Integer getID() {
+		return id;
+	}
+
+	@Override
+	public List<Recipe_I> getRecipes(Database_Support_I db) {
+		List<Recipe_I> result = new ArrayList<Recipe_I>();
+		
+		for(Integer i : Recipes) {
+			result.add(db.getRecipe(i));
+		}
+		
+		return result;
+	}
+
+	@Override
+	public boolean addRecipe(Recipe_I R) {
+		return Recipes.add(R.getID());
 	}
 }
